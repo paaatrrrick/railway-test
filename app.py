ac0edf90c04fbc9904d1363de23eb5bc460c2827
patrick.123.foster@gmail.com <patrick.123.foster@gmail.com>
2023-05-03 11:36:42 -0500

    init2

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+print('This is a test!!!!!!!')
+
 @app.route('/')
 def hello():
+    print('we have been hit')
     return 'Hello, World!'
 
 if __name__ == '__main__':
