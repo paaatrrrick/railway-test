220d1074de9f86772f46cbb4d9889ae66adb4b8a
patrick.123.foster@gmail.com <patrick.123.foster@gmail.com>
2023-05-03 11:42:34 -0500

    port

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 
 app = Flask(__name__)
 
@@ -10,4 +12,4 @@ def hello():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
